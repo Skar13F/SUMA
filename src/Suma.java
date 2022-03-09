96829516fb9b4f07ef21fbd4b6030f0afc98b330
Skar13F <of5162@gmail.com>
2022-03-08 18:11:29 -0600

    primer commit suma

---
+++ src/Suma.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+
+
+public class Suma{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Introduce el primer número");
+		int uno = sc.nextInt();
+		
+		System.out.println("Introduce el segundo número");
+		int dos = sc.nextInt();
+		
+		
+		int resul=uno + dos;
+
+
+		System.out.println("Resultado: "+resul);
+
+        
+	}
+}
